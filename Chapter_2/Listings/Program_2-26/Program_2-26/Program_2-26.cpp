9a04480661ada4fcf360e0cca92957c12469d9fb
Edgar Santana <santanaprogrammer@gmail.com>
2022-06-15 00:29:44 -0400

    Program 2-26

---
+++ Chapter_2/Listings/Program_2-26/Program_2-26/Program_2-26.cpp
@@ -0,0 +1,27 @@
+// Program_2-26.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// PROGRAM: PAYROLL.CPP
+// Written by Herbert Dorfmann
+// This program calculates company payroll
+// Last modification: 8/20/2017
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double payRate;         // Holds the hourly pay rate
+    double hours;           // Holds the hours worked
+    int employNumber;       // Holds the employee number
+    // The remainder of this program is left out
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
