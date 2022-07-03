34f9b844d013125a69fa8a8330321ad9a133aaf6
Edgar Santana <santanaprogrammer@gmail.com>
2022-07-03 01:22:38 -0400

    Update 01-Sum_Of_Two_Numbers.cpp

--- Chapter_2/Programming_Challenges/01-Sum_Of_Two_Numbers/01-Sum_Of_Two_Numbers.cpp
+++ Chapter_2/Programming_Challenges/01-Sum_Of_Two_Numbers/01-Sum_Of_Two_Numbers.cpp
@@ -1,20 +1,19 @@
 // 01-Sum_Of_Two_Numbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+/*
+* Write a program that stores the integers 50 and 100 in variables, 
+and stores the sum of these two in a variable named total.
+*/
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 50;
+    int b = 100;
+    int total = a + b;
+
+    cout << "The total of a + b is: " << total << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
